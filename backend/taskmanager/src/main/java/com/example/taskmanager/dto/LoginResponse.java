cf581746395125e84dd0a28d378e1845c2becec9
kWRizzz <krishnathey2j@gmail.com>
2026-09-06 23:44:43 +0530

    DTO LoginResponse made

---
+++ backend/taskmanager/src/main/java/com/example/taskmanager/dto/LoginResponse.java
@@ -0,0 +1,18 @@
+package com.example.taskmanager.dto;
+
+public class LoginResponse {
+    private String username;
+    private String password;
+
+    public LoginResponse(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
