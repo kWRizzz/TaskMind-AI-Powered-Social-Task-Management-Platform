1815ca2b2002c45d9358e02bc99cd0f13bb7c231
kWRizzz <krishnathey2j@gmail.com>
2026-09-06 23:40:23 +0530

    SecurityConfig mein mene Auth manager ka bean nbanaya hai

--- backend/taskmanager/src/main/java/com/example/taskmanager/config/SecurityConfig.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.example.taskmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,5 +43,12 @@ public class SecurityConfig {
 
         provider.setPasswordEncoder(passwordEncoder);
         return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration
+    ) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 }
