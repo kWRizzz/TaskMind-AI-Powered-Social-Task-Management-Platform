5b55e71f22589f565eee4efba807c3fe3ffbc7f5
kWRizzz <krishnathey2j@gmail.com>
2026-09-06 18:35:21 +0530

    something something

--- backend/taskmanager/src/main/java/com/example/taskmanager/config/SecurityConfig.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/config/SecurityConfig.java
@@ -3,7 +3,10 @@ package com.example.taskmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,5 +30,16 @@ public class SecurityConfig {
                                 );
 
                         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder
+    ){
+        DaoAuthenticationProvider provider= new DaoAuthenticationProvider(userDetailsService);
+
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
     }
 }
