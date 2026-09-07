ffd94c58ab19cda8da8a7f0eb3ed3b02e87e3b5e
kWRizzz <krishnathey2j@gmail.com>
2026-09-07 12:50:17 +0530

    loginResponse autController updated aded tokens

--- backend/taskmanager/src/main/java/com/example/taskmanager/controller/AuthController.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/controller/AuthController.java
@@ -2,8 +2,10 @@ package com.example.taskmanager.controller;
 
 import com.example.taskmanager.dto.LoginRequest;
 import com.example.taskmanager.dto.LoginResponse;
+import com.example.taskmanager.service.JwtService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,23 +13,31 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public AuthController(AuthenticationManager authenticationManager){
+    public AuthController(AuthenticationManager authenticationManager,JwtService jwtService){
         this.authenticationManager=authenticationManager;
+        this.jwtService=jwtService;
     }
 
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request){
-        authenticationManager.authenticate(
+        Authentication authentication= authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getUsername(),
                         request.getPassword()
                 )
         );
 
+        String token=jwtService.generateToken(
+                (org.springframework.security.core.userdetails.UserDetails)
+                     authentication.getPrincipal()
+        );
+
         return new LoginResponse(
                 "Login successful",
-                request.getUsername()
+                request.getUsername(),
+                token
         );
     }
 }
