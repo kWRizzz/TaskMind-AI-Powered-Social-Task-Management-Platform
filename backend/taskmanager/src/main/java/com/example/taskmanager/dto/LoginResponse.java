4d0070a33501b8c24796f0d8c1d73c47e4f36c43
kWRizzz <krishnathey2j@gmail.com>
2026-09-07 12:40:16 +0530

    loginResponse updated icluded tokens

--- backend/taskmanager/src/main/java/com/example/taskmanager/dto/LoginResponse.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/dto/LoginResponse.java
@@ -3,10 +3,16 @@ package com.example.taskmanager.dto;
 public class LoginResponse {
     private String username;
     private String message;
+    private String token;
 
-    public LoginResponse(String username, String message){
+    public LoginResponse(String username, String message ,String token){
         this.message=message;
         this.username=message;
+        this.token=token;
+    }
+
+    public String getToken() {
+        return token;
     }
 
     public String getUsername() {
