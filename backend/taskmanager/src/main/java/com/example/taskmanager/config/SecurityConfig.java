a950d09b1548bf0dff53c5c6e581b3232fa1f04e
kWRizzz <krishnathey2j@gmail.com>
2026-09-09 00:12:28 +0530

    JWTauthFilter dep injected on my sec config

--- backend/taskmanager/src/main/java/com/example/taskmanager/config/SecurityConfig.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.taskmanager.config;
 
 
+import com.example.taskmanager.service.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,9 +13,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class    SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter){
+        this.jwtAuthenticationFilter=jwtAuthenticationFilter;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -29,6 +37,10 @@ public class    SecurityConfig {
                                 .authorizeHttpRequests(auth -> auth
                                         .requestMatchers("/users","/auth/login").permitAll()
                                         .anyRequest().authenticated()
+                                )
+                                .addFilterBefore(
+                                        jwtAuthenticationFilter,
+                                        UsernamePasswordAuthenticationFilter.class
                                 );
 
                         return http.build();
