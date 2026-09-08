f487f7405cd6b852d8ccd99b9711567abc833b1c
kWRizzz <krishnathey2j@gmail.com>
2026-09-09 00:23:57 +0530

    Updated TaskReq

--- backend/taskmanager/src/main/java/com/example/taskmanager/dto/TaskRequest.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/dto/TaskRequest.java
@@ -11,15 +11,15 @@ public class TaskRequest {
     private String title;
     private boolean completed;
 
-    private Long userId;
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
+//    private Long userId;
+//
+//    public Long getUserId() {
+//        return userId;
+//    }
+//
+//    public void setUserId(Long userId) {
+//        this.userId = userId;
+//    }
 
     public TaskRequest(){}
 
