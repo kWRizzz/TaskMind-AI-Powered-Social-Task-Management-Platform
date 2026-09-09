2155d697b7cffec543421156ee497faa153a45dc
kWRizzz <krishnathey2j@gmail.com>
2026-09-10 01:19:55 +0530

    something

---
+++ frontend/src/auth/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div
+        className=' flex justify-center items-center  '
+    >
+
+    </div>
+  )
+}
+
+export default Register
