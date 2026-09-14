d8f1f911d3fc08ea3ed93c34ae4f52dadd5196b8
kWRizzz <krishnathey2j@gmail.com>
2026-09-14 22:31:22 +0530

    Importend updated Task req

--- backend/taskmanager/src/main/java/com/example/taskmanager/dto/TaskRequest.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/dto/TaskRequest.java
@@ -3,23 +3,35 @@ package com.example.taskmanager.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+import java.time.LocalDateTime;
+
 public class TaskRequest {
 
     @NotBlank(message = "Enter some input")
     @Size(min = 3,max = 100,message = "Range of size 3 to 6")
 
     private String title;
-    private boolean completed;
 
-//    private Long userId;
-//
-//    public Long getUserId() {
-//        return userId;
-//    }
-//
-//    public void setUserId(Long userId) {
-//        this.userId = userId;
-//    }
+    @Size(
+            max = 1000,
+            message = "Description must not exceed 1000 char"
+    )
+    private String description;
+
+
+    private String status;
+
+    private String priority;
+
+    private LocalDateTime dueDate;
+
+    private String category;
+
+    @Size(
+            min=1,
+            message = "Estimed time  must be at least 1 min"
+    )
+    private Integer estimatedMinutes;
 
     public TaskRequest(){}
 
@@ -31,12 +43,52 @@ public class TaskRequest {
         this.title = title;
     }
 
-    public boolean isCompleted() {
-        return completed;
+
+    public String getDescription() {
+        return description;
     }
 
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Integer getEstimatedMinutes() {
+        return estimatedMinutes;
+    }
+
+    public void setEstimatedMinutes(Integer estimatedMinutes) {
+        this.estimatedMinutes = estimatedMinutes;
+    }
 }
