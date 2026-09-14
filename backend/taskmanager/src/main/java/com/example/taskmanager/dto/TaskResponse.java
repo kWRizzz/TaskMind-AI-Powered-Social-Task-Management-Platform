3e3b58e14ad295cf812ebadaeb00a2535a0752d6
kWRizzz <krishnathey2j@gmail.com>
2026-09-14 22:41:39 +0530

    Importend updated Task res

--- backend/taskmanager/src/main/java/com/example/taskmanager/dto/TaskResponse.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/dto/TaskResponse.java
@@ -1,15 +1,45 @@
 package com.example.taskmanager.dto;
 
+import com.example.taskmanager.model.TaskPriority;
+import com.example.taskmanager.model.TaskStatus;
+
+import java.time.LocalDateTime;
+
 public class TaskResponse {
 
     private  Long id;
     private String title;
-    private boolean completed;
+    private String description;
+    private TaskPriority priority;
+    private TaskStatus status;
+    private LocalDateTime dueDate;
+    private String category;
+    private Integer estimatedMinutes;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
-    public TaskResponse(Long id, String title, boolean completed){
+    public TaskResponse(
+            Long id,
+            String title,
+            String description,
+            TaskPriority priority,
+            TaskStatus status,
+            LocalDateTime dueDate,
+            String category,
+            Integer estimatedMinutes,
+            LocalDateTime createdAt,
+            LocalDateTime updatedAt
+            ){
         this.id=id;
         this.title=title;
-        this.completed=completed;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.category = category;
+        this.estimatedMinutes = estimatedMinutes;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
     public Long getId() {
@@ -20,7 +50,42 @@ public class TaskResponse {
         return title;
     }
 
-    public boolean isCompleted() {
-        return completed;
+
+    public String getDescription() {
+        return description;
     }
+
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+
+    public String getCategory() {
+        return category;
+    }
+
+
+    public Integer getEstimatedMinutes() {
+        return estimatedMinutes;
+    }
+
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    
+
 }
