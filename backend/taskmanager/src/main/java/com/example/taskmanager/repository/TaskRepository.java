2408438a623390d985892450507beee036589982
kWRizzz <krishnathey2j@gmail.com>
2026-09-20 01:45:04 +0530

    Task repo aded search by priority asnd status

--- backend/taskmanager/src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -1,6 +1,8 @@
 package com.example.taskmanager.repository;
 
 import com.example.taskmanager.model.Task;
+import com.example.taskmanager.model.TaskPriority;
+import com.example.taskmanager.model.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,8 +10,18 @@ import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
     List<Task> findByUserId(Long userId);
+
     List<Task> findByUserIdAndTitleContainingIgnoreCase(
             Long userId,
             String title
     );
+    List<Task> findByUserIdAndStatus(
+            Long userId,
+            TaskStatus status
+    );
+    List<Task> findByUserIdAndPriority(
+            Long userId,
+            TaskPriority priority
+    );
+
 }
