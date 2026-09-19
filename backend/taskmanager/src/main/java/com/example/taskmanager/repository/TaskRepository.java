db0138d349e58313872559e363cf15c401ffe72b
kWRizzz <krishnathey2j@gmail.com>
2026-09-20 01:47:09 +0530

    Task repo extend jpaspeficaorexecuter moving to make its package

--- backend/taskmanager/src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ backend/taskmanager/src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -4,11 +4,12 @@ import com.example.taskmanager.model.Task;
 import com.example.taskmanager.model.TaskPriority;
 import com.example.taskmanager.model.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
 
-public interface TaskRepository extends JpaRepository<Task,Long> {
+public interface TaskRepository extends JpaRepository<Task,Long>, JpaSpecificationExecutor<Task> {
     List<Task> findByUserId(Long userId);
 
     List<Task> findByUserIdAndTitleContainingIgnoreCase(
